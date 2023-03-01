06e8dbc1e9cdaa008619749d2ba22973c07d26c3
Freya Potempa <FreyaPotempa@gmail.com>
2023-03-01 16:57:59 -0600

    picks and previews

--- holiday-road/src/components/itineraries/ItineraryBuilder.js
+++ holiday-road/src/components/itineraries/ItineraryBuilder.js
@@ -5,7 +5,7 @@ import { TripContext } from "./Provider";
 export const ItineraryBuilder = () => {
   const { getParks, parks, getBizs, bizs, getEateries, eateries, getEateryById, getBizById, getParkById, saveNewItinerary, getItineraries, itineraries } = useContext(TripContext);
   const navigate = useNavigate();
-  const [park , setPark] = useState({})
+  const [selectPark , setSelectPark] = useState({})
   const [biz , setBiz] = useState({})
   const [eatery , setEatery] = useState({})
   const [itinerary, setItinerary] = useState(
@@ -23,24 +23,20 @@ export const ItineraryBuilder = () => {
     .then(getEateries())
   }, [])
 
-  //firing on mount which is returning just first object, then fires again but should NOT have to click it twice to make it work
 
-  //run onchange instead of use effect?
-
-  // useEffect(() => {
-  //   if(itinerary.parkId === null){  }
-  //     else{
-  //   getParkById(itinerary.parkId)
-  //   .then(park => setPark(park.data))}
-  //   console.log(park)
-  // },[itinerary])
-  
-//broke it more-- changed onchange in parks select to include get park by id and commented out use effect, so console log stopped returning
+  useEffect(() => {
+    getParkById(itinerary.parkId)
+    .then(newPark => setSelectPark(newPark.data[0]))
+    .then(getBizById(itinerary.bizId)
+    .then(thisBiz => setBiz(thisBiz)))
+    .then(getEateryById(itinerary.eateryId)
+    .then(thisEatery => setEatery(thisEatery)))
+  },[itinerary])
 
   const handleInputItinerary = (event) => {
     const newItinerary = { ...itinerary };
     newItinerary[event.target.id] = event.target.value;
-    setItinerary(newItinerary);
+    setItinerary(newItinerary)
   };
 
   if (parks === 0) {
@@ -51,8 +47,6 @@ export const ItineraryBuilder = () => {
       <h2>List of Parks</h2>
       <select id="parkId" onChange={(event) => {
         handleInputItinerary(event)
-        getParkById(event.target.value)
-        .then(park => setPark(park.data)) 
          }}>      
         <option value="0">Choose Park</option>
         {parks.map((park) => (
@@ -85,11 +79,9 @@ export const ItineraryBuilder = () => {
 
       <h2>Itinerary Preview</h2>
       <div>
-        Park: {
-
-        }
-        Bizarrary: {}
-        Eatery: {}
+        Park: {selectPark.fullName} <button type="button" >Details</button>
+        Bizarrary: {biz.name} <button type="button" >Details</button>
+        Eatery: {eatery.businessName} <button type="button" >Details</button>
       </div>
       <button type="button" onClick={(event) => {saveNewItinerary(itinerary)}}>Save Itinerary</button>
       
